655b02c0bde0b133c199454925d5f045f0c83687
Mohd Kashif <mohdkashif.tech@gmail.com>
2026-06-30 02:08:38 +0530

    Remove API key from firebase config
    Removed API key from firebase configuration for security.

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCe-HHNjKgvJPKxjEbK3wd0c4dBh2YBfiQ",
+    apiKey: "",
     authDomain: "smartstadium-ea49d.firebaseapp.com",
     projectId: "smartstadium-ea49d",
     storageBucket: "smartstadium-ea49d.firebasestorage.app",
